6a7b56fad3208c20737be8bed9b9a10506d20916
Karina <karinacomk.lima@gmail.com>
2019-04-19 15:36:47 -0300

    project restructuring

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
-import { CalendarModule} from "primeng/calendar";
-import { IMaskModule} from "angular-imask";
+import { CalendarModule } from "primeng/calendar";
+import { IMaskModule } from "angular-imask";
+
 
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule
   ]
